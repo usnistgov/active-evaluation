960183a995b4ec303cef0dbeb26c4b6ad62f5fe6
Peter Fontana <peter.fontana@nist.gov>
2020-07-09 21:28:36 -0400

    Updated README

--- docs/_build/_static/documentation_options.js
+++ docs/_build/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2020.03.03',
+    VERSION: 'v2020.04.03_dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
